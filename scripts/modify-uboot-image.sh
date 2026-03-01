7cd1029b9750c0b10acc608cf24104f6f19953d5
xiaomeng9597 <519561910@qq.com>
2026-03-02 07:38:01 +0800

    update modify-uboot-image.sh

--- scripts/modify-uboot-image.sh
+++ scripts/modify-uboot-image.sh
@@ -1,5 +1,4 @@
-# 复制 rk3568-6.x-uboot.img 文件到编译的工作目录
-# cp -f $GITHUB_WORKSPACE/configfiles/rk3568-6.x-uboot.img rk3568-6.x-uboot.img
+# 将 rk3568-6.x-uboot.img 文件打包到相应的镜像文件内
 # mnt/workdir/openwrt/bin/targets/rockchip/armv8/
 
 # 农商云g68 plus
